4fe0075689c7aebd393d8ffd1da56cd5829ff7c8
Ryan Tenorio <ryantenorio@gmail.com>
2018-02-06 20:25:51 -0800

    move old functional code to lib-archive. replace parent directory with new refactored code using Chalice Framework. Supports game end-point using POST paradigm of full body sends for create and update operations.

---
+++ app.py
@@ -0,0 +1,42 @@
+from chalice import Chalice, Response
+from chalice import NotFoundError, BadRequestError
+from chalicelib import games_dao
+
+app = Chalice(app_name='qvolver-db')
+
+
+@app.route('/')
+def index():
+    return {'hello': 'world'}
+
+@app.route('/game', methods=['GET', 'POST'])
+def game():
+    request = app.current_request
+    response = None
+    if request.method == 'GET':
+        response = games_dao.get_all_games()
+        if response == None:
+            raise NotFoundError('No games found')
+    elif request.method == 'POST':
+        new_game = request.json_body
+        response = games_dao.create_game(new_game)
+        if response == None:
+            raise BadRequestError('Bad data submitted')
+    return Response(body=response)
+
+@app.route('/game/{id}', methods=['GET', 'POST', 'DELETE'])
+def get_or_update_game(id):
+    request = app.current_request
+    response = None
+    if request.method == 'GET':
+        response = games_dao.get_game_by_id(id)
+        if response == None:
+            raise NotFoundError(f'Cannot find game for {id}')
+    if request.method == 'POST':
+        game_for_edit = request.json_body
+        response = games_dao.update_game(game_for_edit)
+        if response == None:
+            raise BadRequestError(f'Could not edit game {game_for_edit}')
+    if request.method == 'DELETE':
+        response = games_dao.delete_game_by_id(id)
+    return Response(body=response)
--- run-dynamodb-local.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -Djava.library.path=./DynamoDBLocal_lib -jar ../dynamo-db/DynamoDBLocal.jar -sharedDb -inMemory
